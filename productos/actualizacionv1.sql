ce18494000f8be4eeaf9fdf685509a805b6b51ce
edno2395 <122416439+edno2395@users.noreply.github.com>
2025-09-13 17:03:53 -0500

    Update actualizacionv1.sql

--- productos/actualizacionv1.sql
+++ productos/actualizacionv1.sql
@@ -1,8 +1,16 @@
+-- Crear una nueva tabla
+CREATE TABLE IF NOT EXISTS productos_nuevos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nombre TEXT NOT NULL,
+  descripcion TEXT
+);
+
 -- Agregar una columna a una tabla existente
 ALTER TABLE productos_nuevos ADD COLUMN precio REAL;
 
 -- Insertar algunos registros nuevos
 INSERT INTO productos_nuevos (nombre, descripcion) VALUES ('Producto A', 'Descripción del Producto A');
 INSERT INTO productos_nuevos (nombre, descripcion) VALUES ('Producto B', 'Descripción del Producto B');
+
 
 
